b80e80da1057c338623a88cb99d9d7a9e05ae52f
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-22 14:46:25 -0700

    wrote render function for rpg class cards

--- render-utils.js
+++ render-utils.js
@@ -28,3 +28,43 @@ export function renderAvsPlayer(avs) {
 
     return playerCard;
 }
+
+{/* <div class="rpg-class">
+    <h3>Warrior <img src="https://ubrpbqqozfkjakttjedg.supabase.in/storage/v1/object/sign/rpg-classes/warrior.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJycGctY2xhc3Nlcy93YXJyaW9yLndlYnAiLCJpYXQiOjE2NDc5ODM5OTEsImV4cCI6MTk2MzM0Mzk5MX0.ExXaVy0vwXO7Bgyo6IEXSsBSvoGRiEILLXany42i1UE" alt=""></h3>
+    <ul> Roles:
+        <li>Tank</li>
+        <li>Melee DPS</li>
+    </ul>
+    <ul>Specialization
+        <li>Arms (DPS)</li>
+        <li>Fury (DPS)</li>
+        <li>Protection (Tank)</li>
+    </ul>
+</div> */}
+
+export function renderRpgClass(rpg) {
+    const classCard = document.createElement('div');
+    const nameEl = document.createElement('h3');
+    const imgEl = document.createElement('img');
+    const rolesEl = document.createElement('ul');
+    const specializationsEl = document.createElement('ul');
+
+    classCard.classList.add('rpg-class');
+    nameEl.textContent = rpg.class;
+    imgEl.src = rpg.img;
+    nameEl.append(imgEl);
+    for (let role of rpg.role) {
+        const roleEl = document.createElement('li');
+        roleEl.textContent = role;
+        rolesEl.appendChild(roleEl);
+    }
+    for (let specialization of rpg.specialization) {
+        const specializationEl = document.createElement('li');
+        specializationEl.textContent = specialization;
+        specializationsEl.appendChild(specializationEl);
+    }
+
+    classCard.append(nameEl, rolesEl, specializationsEl);
+
+    return classCard;
+}
